bd8c80f0b96ee5c7f5269549444df80508a1ee97
Bob Schulze <al.lias@gmx.de>
2019-11-21 09:05:40 +0100

    fix for a problem than prevents deletion of a table that is solely custom

--- frappe/model/delete_doc.py
+++ frappe/model/delete_doc.py
@@ -100,7 +100,7 @@ def delete_doc(
 			):
 				try:
 					delete_controllers(name, doc.module)
-				except (OSError, KeyError):
+				except (FileNotFoundError, OSError, KeyError):
 					# in case a doctype doesnt have any controller code  nor any app and module
 					pass
 

d04ec089ca89ed120c0d6a7e301ebe5bfc07c449
Steffen Brennscheidt <steffen@brennscheidt.net>
2020-02-04 16:49:36 +0100

    catch error in context help when not on a doctype

--- frappe/public/js/frappe/help.js
+++ frappe/public/js/frappe/help.js
@@ -25,11 +25,13 @@ function addHelpEntryFromModules(route){
             } else if (route.length > 1){
                 let dt = route[1];
                 let doctypeDoc = frappe.get_doc("DocType", dt);
-                let module = doctypeDoc.module;
-                relevantNotes = notes.filter(function(note){
-                    return !(note['module'] || note['doc_type'])
-                        || note['module'] === module || note['doc_type'] === dt;
-                });
+                if(typeof(doctypeDoc) !== "undefined" && doctypeDoc){
+                    let module = doctypeDoc.module;
+                    relevantNotes = notes.filter(function(note){
+                        return !(note['module'] || note['doc_type'])
+                            || note['module'] === module || note['doc_type'] === dt;
+                    });
+                }
             } else {
                 relevantNotes = notes.filter(function(note){
                     return !(note['module'] || note['doc_type']);

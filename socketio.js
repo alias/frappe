052d71052a72046949e81a5efdf86a8b68312137
Bob Schulze <al.lias@gmx.de>
2021-07-26 17:44:25 +0000

    wild hack to get our cert running at live.pcgteam.net

--- socketio.js
+++ socketio.js
@@ -1,1 +1,8 @@
+
+// we have a problem here, validating the star-ssl cert on the live.pcgteam.net server
+// therefore this ugly hack. Since we are triggering the call from server side, AND any
+// client DID a successful validation on its end anyhow, its probably still safe enough.
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+console.log('WARN: SSL disabled by bob')
+
 require("./realtime");

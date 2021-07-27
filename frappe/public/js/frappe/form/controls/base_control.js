a8d47d4837d6caf9d4c665df4636982a6128f2e5
Steffen <steffen@brennscheidt.net>
2021-07-27 10:26:30 +0200

    feat: Treeview controller can implement which fields are sent to client

--- frappe/public/js/frappe/form/controls/base_control.js
+++ frappe/public/js/frappe/form/controls/base_control.js
@@ -262,7 +262,7 @@ frappe.ui.form.Control = class BaseControl {
 					: this.get_input_value()
 				: undefined;
 		} else {
-			return this.value || undefined;
+			return typeof(this.value) === "undefined" ? undefined : this.value;
 		}
 	}
 	set_model_value(value) {

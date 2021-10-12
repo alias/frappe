47f1eb3db43cfd0855b58efc4e0afa02af743fe4
Steffen <steffen@brennscheidt.net>
2021-10-12 19:14:02 +0200

    fix: error if no 'app_include_svg' found in any of the hooks

--- frappe/www/app.py
+++ frappe/www/app.py
@@ -47,9 +47,10 @@ def get_context(context):
 	include_css = hooks.get("app_include_css", []) + frappe.conf.get("app_include_css", [])
 	
 	svg_string = ""
-	for include_path in hooks["app_include_svg"]:
-		with open(include_path) as file:
-			svg_string += file.read()
+	if "app_include_svg" in hooks:
+		for include_path in hooks["app_include_svg"]:
+			with open(include_path) as file:
+				svg_string += file.read()
 
 	context.update(
 		{

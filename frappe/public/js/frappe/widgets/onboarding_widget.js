39def96ed1f5c046da10c2c0ae8c29d60ee69556
Steffen <steffen@brennscheidt.net>
2021-11-01 17:34:01 +0200

    step specific form tours and workspace menu extensions

--- frappe/public/js/frappe/widgets/onboarding_widget.js
+++ frappe/public/js/frappe/widgets/onboarding_widget.js
@@ -421,6 +421,7 @@ export default class OnboardingWidget extends Widget {
 					});
 					this.mark_complete(step);
 				}
+
 			},
 			null,
 			null,
@@ -440,6 +441,9 @@ export default class OnboardingWidget extends Widget {
 			step.is_complete = true;
 			$step.removeClass("skipped");
 			$step.addClass("complete");
+
+			frappe.desk_page.onboarding_widget.body.empty();
+			frappe.desk_page.onboarding_widget.refresh();
 		};
 
 		this.update_step_status(step, "is_complete", 1, callback);

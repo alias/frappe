fb9b181a37a4c2fa8166100f761da19697d7995c
Steffen <steffen@brennscheidt.net>
2022-01-19 13:18:12 +0100

    rethrow save exception

--- frappe/public/js/frappe/form/form.js
+++ frappe/public/js/frappe/form/form.js
@@ -754,7 +754,9 @@ frappe.ui.form.Form = class FrappeForm {
 				me.show_success_action();
 			})
 			.catch((e) => {
-				console.error(e);
+				console.error(e); // eslint-disable-line
+				throw e;
+
 			});
 	}
 
@@ -1980,7 +1982,6 @@ frappe.ui.form.Form = class FrappeForm {
 		} else {
 			tour_info = frappe.tour[this.doctype];
 		}
-
 
 		if (!Array.isArray(tour_info)) {
 			return;

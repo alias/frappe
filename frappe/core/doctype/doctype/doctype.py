c858f03c49a97d277eb002ca097c060674e0d62a
Till <till.mugele@ooglemail.com>
2022-03-20 12:39:02 +0100

    validate_links_table throw to message

--- frappe/core/doctype/doctype/doctype.py
+++ frappe/core/doctype/doctype/doctype.py
@@ -1193,7 +1193,8 @@ def _test_connection_query(doctype, field, idx):
 		frappe.clear_last_message()
 		msg = _("Document Links Row #{0}: Invalid doctype or fieldname.").format(idx)
 		msg += "<br>" + str(e)
-		frappe.throw(msg, InvalidFieldNameError)
+		# frappe.throw(msg, InvalidFieldNameError)
+		frappe.msgprint(msg)
 
 
 def validate_fields_for_doctype(doctype):

ddc5d43aad2303cb5118b2d7ba068000f5bc2d48
Steffen <steffen@brennscheidt.net>
2022-03-23 21:08:09 +0100

    allow mqsql functions that contain 'concat' string

--- frappe/model/db_query.py
+++ frappe/model/db_query.py
@@ -383,7 +383,6 @@ class DatabaseQuery:
 		"""
 		blacklisted_keywords = ["select", "create", "insert", "delete", "drop", "update", "case", "show"]
 		blacklisted_functions = [
-			"concat",
 			"concat_ws",
 			"if",
 			"ifnull",

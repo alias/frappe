3834036f31a844c49919be1948f1733e8f5e5cfc
Till <till.mugele@ooglemail.com>
2022-08-27 02:16:37 +0200

    add after_document_title_update event

--- frappe/model/rename_doc.py
+++ frappe/model/rename_doc.py
@@ -92,6 +92,7 @@ def update_document_title(
 	if title_updated:
 		try:
 			setattr(doc, title_field, updated_title)
+			doc.run_method("after_document_title_update")
 			doc.save()
 			frappe.msgprint(_("Saved"), alert=True, indicator="green")
 		except Exception as e:

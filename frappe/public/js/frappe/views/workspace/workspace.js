2586fff63e0814e58faef0d9163b6ce95c198a32
Shariq Ansari <sharique.rik@gmail.com>
2022-09-03 15:07:24 +0530

    fix: show unique workspaces in sidebar
    (cherry picked from commit 7331e3a7be0ebbe559a41babe791331eaa86f9f3)

--- frappe/public/js/frappe/views/workspace/workspace.js
+++ frappe/public/js/frappe/views/workspace/workspace.js
@@ -123,6 +123,7 @@ frappe.views.Workspace = class Workspace {
 			}
 
 			if (category === "PCG Live") {
+
 				root_pages = this.public_pages.filter((page) => ["PCG Live", "PCG Web"].includes(page.parent_page));
 			}
 

ad56471c963ef96f24f4348552bdfd129bfc911f
Shariq Ansari <sharique.rik@gmail.com>
2022-10-11 19:06:00 +0530

    fix: format currency value as float while grid upload
    (cherry picked from commit 95f23b3adb6e22c2e7853da8e0cb809b42c903b1)

--- frappe/public/js/frappe/form/grid.js
+++ frappe/public/js/frappe/form/grid.js
@@ -1012,6 +1012,7 @@ export default class Grid {
 				Int: (val) => cint(val),
 				Check: (val) => cint(val),
 				Float: (val) => flt(val),
+				Currency: (val) => flt(val),
 			};
 
 			// upload

bcb7b6963692baf0ff018c5128ed82ec13233993
Steffen <steffen@brennscheidt.net>
2022-11-07 11:50:45 +0100

    fix: show more information on TimestampMismatchError msg

--- frappe/model/document.py
+++ frappe/model/document.py
@@ -862,7 +862,7 @@ class Document(BaseDocument):
 		if cstr(previous.modified) != cstr(self._original_modified):
 			frappe.msgprint(
 				_("Error: Document has been modified after you have opened it")
-				+ (f" ({previous.modified}, {self.modified}). ")
+				+ (f" {self.doctype, self.name} ({previous.modified}, {self.modified}). ")
 				+ _("Please refresh to get the latest document."),
 				raise_exception=frappe.TimestampMismatchError,
 			)

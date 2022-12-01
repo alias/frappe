f2948b88bc28a2f6101e3af5d200db04a69827dd
Till <till.mugele@ooglemail.com>
2022-12-01 15:13:02 +0000

    use occurance checker in send mail

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -20,6 +20,7 @@ import warnings
 from typing import TYPE_CHECKING, Any, Callable, Literal, Optional, overload
 
 import click
+from frappe.pcg_utils.occurance_checker import occurance_checker
 from werkzeug.local import Local, release_local
 
 from frappe.query_builder import (
@@ -650,6 +651,10 @@ def sendmail(
 	:param with_container: Wraps email inside a styled container
 	"""
 
+	context = f'{subject}{sender}{recipients}'
+	if occurance_checker('send mail', context=context, attempt_treshhold=2, expires_in_sec=60*30):
+		return
+	
 	if recipients is None:
 		recipients = []
 	if cc is None:

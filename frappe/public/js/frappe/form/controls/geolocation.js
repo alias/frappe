1ed610428df1c209c537cefac7be777a2ea30b80
alias <dedaluk2>
2023-03-04 10:15:05 +0100

    increase field size

--- frappe/public/js/frappe/form/controls/geolocation.js
+++ frappe/public/js/frappe/form/controls/geolocation.js
@@ -18,7 +18,7 @@ frappe.ui.form.ControlGeolocation = class ControlGeolocation extends frappe.ui.f
 			`<div class="map-wrapper border">
 				<div id="` +
 				this.map_id +
-				`" style="min-height: 400px; z-index: 1; max-width:100%"></div>
+				`" style="min-height: 1000px; z-index: 1; max-width:100%"></div>
 			</div>`
 		);
 		this.map_area.prependTo($input_wrapper);

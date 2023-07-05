6f3580d0c12647e289ef697f2b6332928e34796a
Steffen <steffen@brennscheidt.net>
2023-07-05 20:15:32 +0200

    fix: remove hack to allow everything and fix permission problem with childtables

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -1911,6 +1911,8 @@ def get_list(doctype, *args, **kwargs):
 	        frappe.get_list("ToDo", fields="*", filters = {"description": ("like", "test%")})
 	"""
 	import frappe.model.db_query
+	if is_table(doctype):
+		kwargs['parent_doctype'] = frappe.model.meta.get_parent_dt(doctype)
 
 	return frappe.model.db_query.DatabaseQuery(doctype).execute(*args, **kwargs)
 

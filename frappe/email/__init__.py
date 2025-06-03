9c6e0e5573a670801cc178501f828e1fdf6721d3
Steffen <steffen@brennscheidt.net>
2025-06-03 14:25:43 +0200

    fix: more logging when sending emails
    (cherry picked from commit 3062f9090f799e1b0119e154fe5ccd41d0804037)

--- frappe/email/__init__.py
+++ frappe/email/__init__.py
@@ -194,6 +194,15 @@ def sendmail(
 
 
 	context = f'{subject}{sender}{recipients}'
+
+	def log(msg):
+		logger = frappe.logger('pcg', allow_site=True, max_size=10_000_000 ,file_count=10)
+		logger.setLevel(10) # logging.DEBUG
+		logger.info(msg)
+		print(msg)
+
+	context = f'{subject}{sender}{recipients}'
+	log(f"Sending email to: {recipients} with subject {subject}")
 	if occurance_checker('send mail', context=context, attempt_treshhold=2, expires_in_sec=60*30):
 		return
 	

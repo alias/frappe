819e2f747260964faab967c13ff5f716d7180ace
till <till.mugele@googlemail.com>
2026-04-23 11:47:51 +0000

    remove help entries

--- frappe/hooks.py
+++ frappe/hooks.py
@@ -482,19 +482,7 @@ standard_help_items = [
 		"item_type": "Action",
 		"action": "frappe.ui.toolbar.show_shortcuts(event)",
 		"is_standard": 1,
-	},
-	{
-		"item_label": "System Health",
-		"item_type": "Route",
-		"route": "/desk/system-health-report",
-		"is_standard": 1,
-	},
-	{
-		"item_label": "Frappe Support",
-		"item_type": "Route",
-		"route": "https://frappe.io/support",
-		"is_standard": 1,
-	},
+	}
 ]
 
 # log doctype cleanups to automatically add in log settings
